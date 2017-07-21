984c8155da6def12562f99e1bec2cd8f7cc3a722
Felipe <feload@gmail.com>
2017-07-21 13:43:27 +0000

    Create users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712010924) do
+ActiveRecord::Schema.define(version: 20170721132511) do
 
   create_table "books", force: :cascade do |t|
     t.integer  "client_id"
@@ -29,8 +29,16 @@ ActiveRecord::Schema.define(version: 20170712010924) do
   end
 
   create_table "todos", force: :cascade do |t|
-    t.string "name"
-    t.text   "description"
+    t.string  "name"
+    t.text    "description"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
